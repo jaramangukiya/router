31258ae1fe4239dad46d65fe7d1bff3742055521
jaramangukiya <jaramangukiya@gmail.com>
2022-11-07 08:31:48 +0530

    hiii

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="blogs" element={<Blogs />} />
+          
           <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} />
         </Route>
